708a450f83e0aeabc074df154b035b6a970b1cd2
Vasyl Matsevko <matsevko.vasil@gmail.com>
2021-01-31 18:00:48 +0200

    added and configured gem money-rails for Products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :product do
     name { Faker::GreekPhilosophers.name }
     description { Faker::GreekPhilosophers.quote }
+    price_cents { Faker::Number.number(digits: 5) }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'alidations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price_cents) }
     it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_numericality_of(:price_cents) }
+    it { is_expected.to monetize(:price) }
+    it { is_expected.to monetize(:price).with_currency(:uah) }
   end
 end
